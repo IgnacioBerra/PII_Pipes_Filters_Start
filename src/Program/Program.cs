a207242388780c5c42b7eb31e8f4236f13c82c5c
IgnacioBerra <ignacio.berra@correo.ucu.edu.uy>
2022-10-17 11:00:30 -0300

    Editor de IMAGEN.

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,23 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+            FilterGreyscale greyScale = new FilterGreyscale();
+            FilterNegative negativeFilter = new FilterNegative();
+            PipeNull pipeNull = new PipeNull();
+            PipeSerial pipe2 = new PipeSerial(negativeFilter,pipeNull);
+            PipeSerial pipe = new PipeSerial(greyScale,pipe2);
+           
+        
+            IPicture image = pipe.Send(picture);
+            provider.SavePicture(image, @"EditLuke.jpg");
+
+            
+
+
+            
+            provider.SavePicture(image, @"PathToImageToSave.jpg");
         }
     }
 }
